bd15af333e7d23256e39564849026f4afbfea774
syeenakim <syeenakim@gmail.com>
2022-12-30 16:12:51 +0900

    20221230

--- web-cluster/main.tf
+++ web-cluster/main.tf
@@ -14,22 +14,22 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_launch_template" "example" {
 
+# 시작 템플릿 구성
+resource "aws_launch_template" "example" {
   name                   = "std02-example"
   image_id               = "ami-06eea3cd85e2db8ce"
   instance_type          = "t2.micro"
   key_name               = "std02-key"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-	user_data = "${base64encode(data.template_file.web_output.rendered)}"
-
+  user_data = base64encode(data.template_file.web_output.rendered)
 
   lifecycle {
     create_before_destroy = true
   }
 }
-
+# 오토스케일링 그룹생성
 resource "aws_autoscaling_group" "example" {
   availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
 
@@ -37,6 +37,9 @@ resource "aws_autoscaling_group" "example" {
   desired_capacity = 1
   min_size         = 1
   max_size         = 2
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   launch_template {
     id      = aws_launch_template.example.id
@@ -49,6 +52,68 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
+#로드밸런서 생성
+resource "aws_lb" "example" {
+  name               = "std02-terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# 대상그룹 생성
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+#로드밸런서 리스너 구성
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+#로드밸런서 리스너 규칙 생성
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority    = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+#인스턴스 보안그룹,8080오픈
 
 resource "aws_security_group" "instance" {
   name = var.security_group_name
@@ -57,6 +122,25 @@ resource "aws_security_group" "instance" {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 로드밸런서 보안그룹, 80오픈
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -72,9 +156,9 @@ data "aws_subnets" "default" {
   }
 }
 
-data "template_file" "web_output"{
-	template = file("${path.module}/web.sh")
-	vars = {
-		server_port = var.server_port
-	}
+data "template_file" "web_output" {
+  template = file("${path.module}/web.sh")
+  vars = {
+    server_port = var.server_port
+  }
 }
